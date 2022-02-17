7592753f8f4d9ffbbb2dc33a0e78d0f5af721374
quasar098 <patellmlg@gmail.com>
2022-02-17 09:15:48 -0500

    added images note: needs live server for loading images

--- javascript/master.js
+++ javascript/master.js
@@ -1,14 +1,22 @@
 
+let img;
+
+function preload() {
+	img = loadImage('javascript/images/hat.png');
+}
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	image(img, 0, 0)
 }
 
 function draw() {
-	clear()
-	circle(windowWidth/2, windowHeight/2, 50)
+	clear();
+	circle(windowWidth/2, windowHeight/2, 50);
 }
 
 // makes it possible to resize the window properly
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+
 }
