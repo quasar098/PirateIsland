0427a112df63ba81c8b15d0e2221ee0c26972661
quasar098 <patellmlg@gmail.com>
2022-02-17 09:17:02 -0500

    fixed image laoding

--- javascript/master.js
+++ javascript/master.js
@@ -2,17 +2,17 @@
 let img;
 
 function preload() {
-	img = loadImage('javascript/images/hat.png');
+	img = loadImage('javascript/images/character/idle.png');
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	image(img, 0, 0)
 }
 
 function draw() {
 	clear();
 	circle(windowWidth/2, windowHeight/2, 50);
+	image(img, 0, 0);
 }
 
 // makes it possible to resize the window properly
